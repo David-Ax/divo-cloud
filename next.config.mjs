a00318c784727f3618f079d161702c9e52b07f91
davidax <davidax204@gmail.com>
2025-04-17 18:48:33 +0200

    :hammer: design

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  middleWare: true,
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
